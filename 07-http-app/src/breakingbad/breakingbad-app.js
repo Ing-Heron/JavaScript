d1ceeafb840cd2636a260c8d326f3f7fe6157022
Ing-Heron <94878709+Ing-Heron@users.noreply.github.com>
2025-03-26 08:28:29 -0600

    Adding some works of seccion 13

--- 07-http-app/src/breakingbad/breakingbad-app.js
+++ 07-http-app/src/breakingbad/breakingbad-app.js
@@ -5,8 +5,7 @@
 const fetchQoute = async() => {
     const res = await fetch( 'https://api.breakingbadquotes.xyz/v1/quotes' );
     const data = await res.json();
-
-    console.log( data[0] );
+    
     return data[0];
 }
 
