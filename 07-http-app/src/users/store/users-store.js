c6f6fba04f59c1b47956425da871077bf01e3097
Ing-Heron <94878709+Ing-Heron@users.noreply.github.com>
2025-04-04 16:52:53 -0600

    Adding seccion 13 CRUD App

--- 07-http-app/src/users/store/users-store.js
+++ 07-http-app/src/users/store/users-store.js
@@ -1,5 +1,7 @@
 import { loadUsersByPage } from "../use-cases/load-users-by-page";
 
+let previousID;
+
 const state = {
     currentPage: 0,
     users: [],
@@ -7,17 +9,36 @@ const state = {
 
 const loadNextPage = async() => {
     const users = await loadUsersByPage( state.currentPage + 1 );
+    if ( users[users.length-1].id === previousID ) return;
     if ( users.length === 0 ) return;
     state.currentPage += 1;
     state.users = users;
+    previousID = users[users.length-1].id;
 }
 
 const loadPreviousPage = async() => {
-    throw new Error('No implementado');
+    if ( state.currentPage === 1 ) return;
+    const users = await loadUsersByPage( state.currentPage - 1 );
+    state.currentPage -=1;
+    state.users = users;
 }
 
-const onUserChanged = () => {
-    throw new Error('No implementado');
+const onUserChanged = ( updatedUser ) => {
+
+    let wasFound = false;
+    
+    state.users = state.users.map( user => {
+        if ( user.id === updatedUser.id ) {
+            wasFound = true;
+            return updatedUser;
+        }
+        
+        if ( state.users.length < 10 && !wasFound ) {
+            state.users.push( updatedUser );
+        }
+        return user;
+    });
+
 }
 
 const reloadPage = async() => {
@@ -30,6 +51,15 @@ export default {
     onUserChanged,
     reloadPage,
 
+    /**
+     * 
+     * @returns {User[]}
+     */
     getUsers: () => [...state.users],
+
+    /**
+     * 
+     * @returns {Number}
+     */
     getCurrentPage: () => state.currentPage,
 }
--- 07-http-app/src/users/users-app.js
+++ 07-http-app/src/users/users-app.js
@@ -1,4 +1,9 @@
+import { renderAddButton } from "./presentation/render-add-button/render-add-button";
+import { renderButtons } from "./presentation/render-buttons/render-buttons";
+import { renderModal } from "./presentation/render-modal/render-modal";
+import { renderTable } from "./presentation/render-table/render-table";
 import usersStore from "./store/users-store";
+import { saveUser } from "./use-cases/save-user";
 
 /**
  * 
@@ -8,6 +13,16 @@ export const UsersApp = async( element ) => {
 
         element.innerHTML = 'Loading...';
         await usersStore.loadNextPage();
+        element.innerHTML = '';
 
-        console.log( usersStore.getUsers());
+        renderTable( element );
+        renderButtons( element );
+        renderAddButton( element); 
+        renderModal( element, async ( userLike ) => {
+                const user = saveUser( userLike );
+                usersStore.onUserChanged( user );
+                renderTable();
+        });
+
+        
 }
